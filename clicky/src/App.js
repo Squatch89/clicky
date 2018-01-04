bf9b3b89a3c05686d0af40cc216bdb4e7f96792c
Gene Williams <GeneWillb@Gmail.com>
2018-01-04 14:40:59 -0500

    added shuffle for images

--- clicky/src/App.js
+++ clicky/src/App.js
@@ -13,6 +13,12 @@ class App extends Component {
         image: ["http://image-cdn.neatoshop.com/styleimg/62796/none/kiwigreen/default/361729-19;1512463610i.jpg", "https://www.geek.com/wp-content/uploads/2016/05/squanching.jpg", "https://vignette.wikia.nocookie.net/rickandmorty/images/0/08/Tammy.png/revision/latest?cb=20140523063749", "https://vignette.wikia.nocookie.net/rickandmorty/images/b/bd/Shut_Up_Hamurai.png/revision/latest?cb=20170131022414", "https://vignette.wikia.nocookie.net/rickandmorty/images/d/d2/Rick_D99.png/revision/latest?cb=20170402053917", "https://vignette.wikia.nocookie.net/rickandmorty/images/8/84/Fart.jpg/revision/latest?cb=20150806021947", "http://static.tvtropes.org/pmwiki/pub/images/snuffles_helmet.jpg", "http://cdn.smosh.com/sites/default/files/2017/09/mr%20poopy%20butthole.jpg", "http://www.rickandmortyshow.com/wp-content/uploads/2016/07/summer2.jpg", "https://thoughtcatalog.files.wordpress.com/2017/11/rick-sanchez-and-morty-adult-swim.jpg?w=590&h=383", "http://static.tvtropes.org/pmwiki/pub/images/i2h2pgn.png", "http://static.tvtropes.org/pmwiki/pub/images/13_1.jpg"]
     };
     
+    sortImages = () => {
+        
+        const imageOrder = this.state.image.sort(() => Math.random() - 0.5);
+        
+        this.setState(imageOrder);
+    };
     
     
   render() {
@@ -23,6 +29,7 @@ class App extends Component {
           <GameSpace>
           {this.state.image.map(image => (
               <Images
+                  sortImages={this.sortImages}
                   src={image}
                   clicked={false}
               />
--- clicky/src/components/Images/Image.js
+++ clicky/src/components/Images/Image.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Image.css';
 
 const Image = props => (
-    <div className='img-thumbnail imgWrapper'>
+    <div onClick={() => props.sortImages()} className='img-thumbnail imgWrapper'>
         <img src={props.src} className='img-fluid imgSize gridEle ' alt="rick and morty character"/>
     </div>
 );
